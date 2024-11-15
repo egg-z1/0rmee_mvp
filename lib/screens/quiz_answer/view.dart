036bbd184a980c6ec24d4c41274aecc585113144
veronica2550 <128062548+veronica2550@users.noreply.github.com>
2024-11-16 01:29:09 +0900

    Add screens

---
+++ lib/screens/quiz_answer/view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuizAnswer extends StatelessWidget {
+  const QuizAnswer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
