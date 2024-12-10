4db4a32e563a866de67507caf442e367bd50fefe
veronica2550 <128062548+veronica2550@users.noreply.github.com>
2024-12-10 18:14:12 +0900

    feat: 퀴즈 데이터 호출(#17)

---
+++ lib/screens/quiz/model.dart
@@ -0,0 +1,31 @@
+class QuizCard {
+  final String id;
+  final String content;
+  final String type;
+  final String answer;
+  final List<String?> items;
+  final bool? submission;
+  final bool? isCorrect;
+
+  QuizCard({
+    required this.id,
+    required this.content,
+    required this.type,
+    required this.answer,
+    required this.items,
+    this.submission,
+    this.isCorrect,
+  });
+
+  factory QuizCard.fromJson(Map<String, dynamic> json) {
+    return QuizCard(
+      id: json['id'].toString(),
+      content: json['content'],
+      type: json['type'],
+      answer: json['answer'],
+      items: List<String>.from(json['items'] ?? []),
+      submission: json['submission'],
+      isCorrect: json['isCorrect'],
+    );
+  }
+}
---
+++ lib/screens/quiz/service.dart
@@ -0,0 +1,57 @@
+import 'package:get/get.dart';
+import 'package:ormee_mvp/api/OrmeeApi.dart';
+import 'package:ormee_mvp/screens/quiz/model.dart';
+
+class QuizService extends GetConnect {
+  QuizService() {
+    // 기본 URL 및 타임아웃 설정
+    httpClient.baseUrl = '${API.hostConnect}';
+    httpClient.timeout = const Duration(seconds: 10);
+
+    // 요청 인터셉터: 헤더 설정 및 요청 로깅
+    httpClient.addRequestModifier<dynamic>((request) async {
+      // print('🌐 Request: [${request.method}] ${request.url}');
+      request.headers['Accept'] = 'application/json';
+      request.headers['Content-Type'] = 'application/json; charset=utf-8';
+      return request;
+    });
+  }
+
+  /// 강의 상세 정보 가져오기
+  Future<List<QuizCard>> fetchLectureDetail(String quizId) async {
+    final String url = '/quizes/$quizId';
+    try {
+      // GET 요청 전송
+      final response = await get(url);
+
+      // 상태 코드 확인 (200~299은 성공)
+      if (response.isOk && response.body != null) {
+        final body = response.body;
+        // print('Body Type: ${body.runtimeType}');
+        // print('Body: $body');
+
+        // 응답 데이터 구조 확인 및 파싱
+        if (body is Map<String, dynamic> &&
+            body['status'] == 'success' &&
+            body['code'] == 200 &&
+            body['data'] != null) {
+          List<QuizCard> quizList = (body['data'] as List)
+              .map((item) => QuizCard.fromJson(item))
+              .toList();
+          // print(quizList);
+          return quizList;
+        }
+
+        throw Exception('Invalid response format: ${response.bodyString}');
+      }
+
+      // 응답 실패 처리
+      throw Exception(
+          'Request failed: [${response.statusCode}] ${response.bodyString}');
+    } catch (e, stackTrace) {
+      // print('❌ Error in fetchLectureDetail: $e');
+      // print('Stack trace: $stackTrace');
+      rethrow;
+    }
+  }
+}
