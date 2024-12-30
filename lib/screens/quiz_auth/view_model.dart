eed97ec730246c0fcf8d70868c659d422479b122
veronica2550 <128062548+veronica2550@users.noreply.github.com>
2024-12-30 12:15:39 +0900

    feat: 퀴즈 사용자 정보 검증 API 연결(#47)

--- lib/screens/quiz_auth/view_model.dart
+++ lib/screens/quiz_auth/view_model.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ormee_mvp/screens/quiz_auth/service.dart';
 
 class QuizAuthController extends GetxController {
   final focusNode = FocusNode();
   final textController = TextEditingController();
   final isTextFieldNotEmpty = false.obs;
+  final QuizAuthService _service = QuizAuthService();
+
+  var isLoading = false.obs;
+  var userInfoValidate = Rx<bool?>(null);
+  var error = Rx<String?>(null);
 
   @override
   void onClose() {
     focusNode.dispose();
     textController.dispose();
     super.onClose();
+  }
+
+  Future<void> fetchUserInfoValidate(
+      String quizId, String author, String password) async {
+    isLoading(true);
+    error(null);
+
+    try {
+      final detail =
+          await _service.fetchUserInfoValidate(quizId, author, password);
+      userInfoValidate.value = detail;
+    } catch (e) {
+      error(e.toString());
+      Get.snackbar(
+        '오류',
+        '사용자 검증을 실패했습니다.',
+        snackPosition: SnackPosition.BOTTOM,
+        duration: const Duration(seconds: 3),
+      );
+    } finally {
+      isLoading(false);
+    }
   }
 }
