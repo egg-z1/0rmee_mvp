53290d44a8825da345729322c213f38430feff9b
veronica2550 <128062548+veronica2550@users.noreply.github.com>
2024-12-30 16:50:12 +0900

    chore: 안 쓰는 변수 제거(#73)

--- lib/screens/lecture_detail/view.dart
+++ lib/screens/lecture_detail/view.dart
@@ -27,7 +27,6 @@ class _LectureDetailState extends State<LectureDetail> {
   final LectureController controller = Get.put(LectureController());
   final TextEditingController _controller = TextEditingController();
   final isTextFieldNotEmpty = false.obs;
-  OverlayEntry? overlayEntry;
 
   @override
   void initState() {
@@ -45,7 +44,6 @@ class _LectureDetailState extends State<LectureDetail> {
   void dispose() {
     _controller.removeListener(_onTextChanged);
     _controller.dispose();
-    overlayEntry?.remove();
     super.dispose();
   }
 
@@ -207,7 +205,6 @@ class _LectureDetailState extends State<LectureDetail> {
                                 child: GestureDetector(
                                   behavior: HitTestBehavior.translucent,
                                   onTap: () {
-                                    overlayEntry?.remove();
                                     Get.to(QuizAuth(
                                       quizId: quiz.id,
                                       quizTitle: quiz.quizName,
